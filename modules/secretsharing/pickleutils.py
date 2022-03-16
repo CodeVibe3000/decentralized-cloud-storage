71c6e857a3f278d320133d4b19044325d4fbb66b
Vedant Valluri <vedant.valluri@gmail.com>
2022-03-16 09:06:14 -0700

    added better compression

--- modules/secretsharing/pickleutils.py
+++ modules/secretsharing/pickleutils.py
@@ -3,10 +3,10 @@ import pickle
 import _pickle as cPickle
 
 def compressed_pickle(title, data):
-    with bz2.BZ2File(title + '.pbz2', 'w') as f: 
+    with bz2.BZ2File(title + '.pbz2', 'w', compresslevel=9) as f: 
         cPickle.dump(data, f)
 
 def decompress_pickle(file):
-    data = bz2.BZ2File(file + '.pbz2', 'rb')
+    data = bz2.BZ2File(file + '.pbz2', 'rb', compresslevel=9)
     data = cPickle.load(data)
     return data
